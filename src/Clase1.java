d477b93cf6641b8be886420871df70811a51f16d
agustinacedo07 <agus.chap7@gmail.com>
2018-03-06 09:13:32 +0100

    Commit Nueva versión 1.2

--- src/Clase1.java
+++ src/Clase1.java
@@ -2,7 +2,7 @@
 public class Clase1 {
 	public static void main(String[] args) {
 		System.out.println("Hola");
-		System.out.println("Nueva Version");
+		System.out.println("Nueva Version 1.2");
 	}
 
 }
